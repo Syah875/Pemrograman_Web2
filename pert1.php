bfaeb0f3c74ee87c79eca7c8e795f81ee2dda611
Syah875 <107460723+Syah875@users.noreply.github.com>
2024-03-05 20:25:14 +0700

    pertemuan1
    pertemuan1

---
+++ pert1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>biodata php</title>
+</head>
+<body>
+    <?php
+        print"<pre>";
+        print"<center><b><font size=14 color='red'>BIODATA</font></b></center>";
+        print"<font size=12 color='blue'>Nama : Hardiansyah</font><br>";
+        print"<font size=12 color='blue'>Email : hardiansyah875@gmail.com</font><br>";
+        print"<font size=12 color='blue'>Jurusan :  Teknik Informatika</font><br>";
+        print"<font size=12 color='blue'>Hobi : Memancing</font><br></pre>";
+        
+    ?>
+</body>
+</html>
